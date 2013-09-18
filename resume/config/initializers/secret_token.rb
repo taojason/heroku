5627e1590858f1344ec50e914b76f3ad30924f1b
j23tao <taojason@gmail.com>
2013-09-18 10:37:30 -0700

    Initial commit

---
+++ resume/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resume::Application.config.secret_token = '0c5482a5e5b1154d6f2f12853e0efc338cac326b664dfbf013cf05824554b4600ce422b2dbbaa565d4aeb9f0793c8e7a83ad7df046cc760c18d55acee9de8201'
