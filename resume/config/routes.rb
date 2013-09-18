20cd1ab98d546362ec908383400508d651cfe4ab
j23tao <taojason@gmail.com>
2013-09-18 11:07:44 -0700

    finish static-pages

--- resume/config/routes.rb
+++ resume/config/routes.rb
@@ -1,4 +1,11 @@
 Resume::Application.routes.draw do
+
+  root to: 'static_pages#home'
+
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+
+  match '/signup', to: 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
